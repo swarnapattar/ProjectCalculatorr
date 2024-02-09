92bf5ed0efa810c39982884bf636be03547c32c7
swarnapattar <157886176+swarnapattar@users.noreply.github.com>
2024-02-09 21:03:38 +0530

    Extended code for arith ops

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+// Arithmetic.java
+public class arithmetic extends fib {
+    int cube(int a){
+       return a * a * a;
+    }
+    int square(int a){
+         return a * a;
+    }
+    int modulus(int a, int b){
+        return a % b;
+    }
+    int power(int a, int b){
+        return (int) Math.pow(a, b);
+    }
+}
